4f9adf72a2374aaa1446c7d6a3f979cb415d54ea
mstier99 <82499841+mstier99@users.noreply.github.com>
2023-08-21 12:11:05 +0200

    add

--- RabbitMq/Send/Send.cs
+++ RabbitMq/Send/Send.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare
+(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish
+(
+    exchange: "",
+    routingKey: "hello",
+    basicProperties: null,
+    body: body
+);
+
+Console.WriteLine("Sent message!");
+Console.WriteLine("Press [enter] to exit.");
+
+Console.ReadKey();
